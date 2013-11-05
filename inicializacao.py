4d4074405288a115ea5876cc507c0c860153e458
Rafael Pereira <rafaelrps11@gmail.com>
2013-11-04 22:10:05 -0200

    Alteração nos Graficos && Inicialização do MySQL e Django pelo arquivo de inicializacao.py

--- inicializacao.py
+++ inicializacao.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
+import os
 
+os.system('sudo /opt/lampp/lampp start')
+os.system('python manage.py runserver')
 
-
-cd /opt/lampp
-ls
-
+#Abre o projeto no Firefox
+#os.system('firefox localhost:8000')
